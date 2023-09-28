3339eaf74a2791ded2a4196e5c1f75e26b2beddc
kasia.kordys <kasia.kordys@gmail.com>
2023-09-28 22:12:57 +0300

    fix spinner

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
     <div className="d-flex justify-content-center">
-      <div className="spoinner-border" role="status">
+      <div className="spinner-border" role="status">
         <span className="sr-only"></span>
       </div>
     </div>
