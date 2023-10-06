5951ed4b8c28bc3b982a3d2bbe87f1fa5efb8143
kasia.kordys <kasia.kordys@gmail.com>
2023-10-06 16:50:51 +0300

    add client info to project

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
+import ClientInfo from "../components/ClientInfo";
 
 export default function Project() {
   //gets id from the url
@@ -26,6 +27,7 @@ export default function Project() {
           <p>{data.project.description}</p>
           <h5 className="mt-3">Project Status</h5>
           <p className="lead">{data.project.status}</p>
+          <ClientInfo client={data.project.client} />
         </div>
       )}
     </>
